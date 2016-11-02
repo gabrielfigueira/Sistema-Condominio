0cc15f4432102ad2ec4c37d4fb9d6cc6bc2b68c0
Gabriel Figueira <figueira.gabriel11@gmail.com>
2016-11-02 16:57:19 -0200

    Criado Botão para teste de inserção con entity

--- Sistema Condominio/Form1.cs
+++ Sistema Condominio/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sistema_Condominio.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,29 @@ namespace Sistema_Condominio
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BancoDeDados banco = new BancoDeDados();
+
+            banco.pessoa.Add(new pessoa()
+            {
+                ID = 1,
+                NOME = "Gabriel",
+                CPF = "321321",
+                RG = "312312321",
+                TELEFONE_RESIDENCIAL = "312321",
+                TEL_COMERC = "23112231",
+                CELULAR = "23312321",
+                EMAIL = "gabriel@gabriel.com",
+                NECES_ESPECIAL = true
+            }); 
+                       
+            banco.SaveChanges();
+
+            MessageBox.Show("Salvei");
+
         }
     }
 }
